15cfb6252dbbf5198e214199285b65edc99f7e70
Danyaal Majid <71455068+DanyaalMajid@users.noreply.github.com>
2024-02-26 07:59:37 +0000

    Implement BlackJack Scoring

--- blackjack.py
+++ blackjack.py
@@ -7,10 +7,23 @@ class blackjack:
         self.player_hand = []
         self.dealer_hand = []
         self.player_score = 0
+        self.player_money = 0
+        self.player_bet_amount = 0
         self.dealer_score = 0
         self.game_over = False
         self.deal_cards()
 
+    def new_game_init(self):
+        self.deck = self.create_deck()
+        self.player_hand = []
+        self.dealer_hand = []
+        self.player_score = 0
+        self.dealer_score = 0
+        self.player_bet_amount = 0
+        self.game_over = False
+        self.deal_cards()
+
+    
     def create_deck(self):
         # Create a deck of cards
         suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
@@ -66,6 +79,8 @@ class blackjack:
         for card in self.player_hand:
             print(f"{card['rank']} of {card['suit']}")
         print(f"Score: {self.player_score}")
+        print(f"Money: {self.player_money}")
+        print(f"Bet: {self.player_bet_amount}")
         print("\nDealer's Hand:")
         if show_dealer_hand:
             for card in self.dealer_hand:
@@ -81,10 +96,13 @@ class blackjack:
             self.print_board()
             if self.player_score == 21:
                 print("Blackjack! You win!")
+                self.player_money += self.player_bet_amount * 2
+                self.player_bet_amount = 0
                 self.game_over = True
                 break
             elif self.player_score > 21:
                 print("Bust! You lose!")
+                self.player_bet_amount = 0
                 self.game_over = True
                 break
             else:
@@ -103,28 +121,56 @@ class blackjack:
         self.print_board(show_dealer_hand=True)
         if self.dealer_score == 21:
             print("Blackjack! Dealer wins!")
+            self.player_bet_amount = 0
         elif self.dealer_score > 21:
             print("Bust! Dealer loses!")
+            self.player_money += self.player_bet_amount * 2
+            self.player_bet_amount = 0
         elif self.dealer_score > self.player_score:
             print("Dealer wins!")
+            self.player_bet_amount = 0
         elif self.dealer_score < self.player_score:
             print("Player wins!")
+            self.player_money += self.player_bet_amount * 2
+            self.player_bet_amount = 0
         else:
             print("It's a tie!")
+            self.player_money += self.player_bet_amount
+            self.player_bet_amount = 0
         self.game_over = True
 
-    def play_game(self):
+    def player_intial_bet(self):
+        self.player_money = int(input("Enter the amount of money you want to start with: "))
+        self.player_bet()
+
+    def player_bet(self):
+        print(f"Your current money: {self.player_money}")
+        self.player_bet_amount = int(input("Enter the amount of money you want to bet: "))
+        if self.player_bet_amount > self.player_money:
+            print("You don't have enough money to bet that amount!")
+            self.player_bet()
+
+        else:
+            self.player_money -= self.player_bet_amount
+
+    def play_game(self, new_game=True):
         # Main game loop
+        if new_game == True:
+            self.player_intial_bet()
+        else:
+            self.player_bet()
+
         while not self.game_over:
             self.player_turn()
             if not self.game_over:
                 self.dealer_turn()
         play_again = input("Do you want to play again? (y/n): ")
         if play_again == 'y':
-            self.__init__()
-            self.play_game()
+            self.new_game_init()  # Reset game except player_money
+            self.play_game(new_game=False)  # Start a new game
         else:
             print("Thanks for playing!")
+            print(f"Your final money: {self.player_money}")
 
 if __name__ == "__main__":
     game = blackjack()
